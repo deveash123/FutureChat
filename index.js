3606a9f97024593a6038a59b8e9c1eba0676a2df
deveash <gdeveash@gmail.com>
2026-01-05 11:38:10 +0530

    Added photo sharing

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 
-// --- THIS IS THE NEW LINE ---
-app.use(express.static(__dirname)); 
-// ----------------------------
+// UPDATE: We increase the limit to 10MB so photos can pass through
+const io = new Server(server, {
+    maxHttpBufferSize: 1e8 // 100 MB limit
+});
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  // We don't need to log every connection, keeps the terminal clean
+  
   socket.on('chat message', (msg) => {
+    // Broadcast the message (text or image) to everyone
     io.emit('chat message', msg);
   });
 });
