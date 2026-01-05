4db805d40ab91db6d1b0b206afcdf02e7c0f1f9d
deveash <gdeveash@gmail.com>
2026-01-05 11:28:20 +0530

    Allow server to share manifest

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// 1. Show the Website when someone visits
+// --- THIS IS THE NEW LINE ---
+app.use(express.static(__dirname)); 
+// ----------------------------
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('⚡ A user connected!');
-
-  // 2. Listen for chat messages
-socket.on('chat message', (msg) => {
-  io.emit('chat message', msg); // <--- This sends it to EVERYONE
-});
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('listening on *:3000');
 });
